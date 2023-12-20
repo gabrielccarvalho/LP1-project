292715929d0a2904f353baf96c75a7b6a7fb0064
Gabriel <caampos22@gmail.com>
2023-12-20 18:37:00 -0300

    Feat ✨: remove student.cpp

--- Student.h
+++ Student.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <memory>
 #include <cstring>
+#include <algorithm>
 
 class Person {
 public:
@@ -45,3 +46,31 @@ private:
   int registrationID;
   std::vector<std::string> classes;
 };
+
+std::string Student::getName() {
+  return Person::getName();
+}
+
+int Student::getAge() {
+  return Person::getAge();
+}
+
+int Student::getRegistrationID() {
+  return this->registrationID;
+}
+
+std::vector<std::string> Student::getClasses() {
+  return this->classes;
+}
+
+void Student::addClass(std::string className) {
+  this->classes.push_back(className);
+}
+
+void Student::editClass(std::string oldClassName, std::string newClassName) {
+  std::replace(this->classes.begin(), this->classes.end(), oldClassName, newClassName);
+}
+
+void Student::removeClass(std::string className) {
+  this->classes.erase(std::remove(this->classes.begin(), this->classes.end(), className), this->classes.end());
+}
